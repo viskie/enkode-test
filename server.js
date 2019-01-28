fa09a46ed10c255f1fa422ab2fba03412a54c1a9
Vishak <vishak@vishak.me>
2019-01-28 15:57:22 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
